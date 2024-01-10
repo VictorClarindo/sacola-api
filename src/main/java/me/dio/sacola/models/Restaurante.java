8fa4f7edd04ba4c5ab04eaf12d03c9e9eb5706e5
Victor Clarindo <victorclarindo77@gmail.com>
2024-01-09 23:47:36 -0300

    Initial commit

---
+++ src/main/java/me/dio/sacola/models/Restaurante.java
@@ -0,0 +1,29 @@
+package me.dio.sacola.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.dio.sacola.models.Endereco;
+import me.dio.sacola.models.Produto;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
+public class Restaurante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Produto> cardapio;
+    @Embedded
+    private Endereco endereco;
+}
